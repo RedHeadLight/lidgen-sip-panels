6eb27a1e69386e903c795440b85f762a4be3d8bf
Andrey Belkin <belkin00733@gmail.com>
2024-05-23 13:05:51 +0200

    попап

--- src/js/app.js
+++ src/js/app.js
@@ -13,23 +13,23 @@ document.addEventListener('DOMContentLoaded', function(){ // Аналог $(docu
         })
     })
     
-    let openModalBtns = document.querySelectorAll(".open-modal") 
-    let modal = document.querySelector(".modal")
-    let modalOverlay = document.querySelector(".modal-overlay")
-    
-    openModalBtns.forEach((item) => {
-        item.addEventListener("click", () => {
-            modal.classList.remove("modal--hidden")
-            modalOverlay.classList.remove("modal-overlay--hidden")
-        })
-    })
-    
-    let modalCloseBtn = document.querySelector(".modal__close")
-    
-    modalCloseBtn.addEventListener("click", () => {
-        modal.classList.add("modal--hidden")
-        modalOverlay.classList.add("modal-overlay--hidden")    
-    })    
+    // let openModalBtns = document.querySelectorAll(".open-modal")
+    // let modal = document.querySelector(".modal")
+    // let modalOverlay = document.querySelector(".modal-overlay")
+    //
+    // openModalBtns.forEach((item) => {
+    //     item.addEventListener("click", () => {
+    //         modal.classList.remove("modal--hidden")
+    //         modalOverlay.classList.remove("modal-overlay--hidden")
+    //     })
+    // })
+    //
+    // let modalCloseBtn = document.querySelector(".modal__close")
+    //
+    // modalCloseBtn.addEventListener("click", () => {
+    //     modal.classList.add("modal--hidden")
+    //     modalOverlay.classList.add("modal-overlay--hidden")
+    // })
 });
 
 
@@ -41,5 +41,5 @@ document.addEventListener('DOMContentLoaded', function(){ // Аналог $(docu
 headerFixed()
 // =======================================================================================================
 
-// togglePopupWindows()
+togglePopupWindows()
 // =======================================================================================================
